6dd0d9abe64d846ea8c484028e0676def112e0ba
gray_jiang <Gray_Jy@yeah.net>
2023-11-11 00:41:19 +0800

    ✨ feat(Raffle.sol): get randomWords

--- README.md
+++ README.md
@@ -9,4 +9,12 @@ This code is to create a provable random smart contract lottery.
 1. Users can enter by paying for a ticket,and the ticket fee are going to the winner during the draw.
 2. After X period of time,the lottery will automatically draw a winner,and this will be done programmatically.
 3. Using Chainlink VRF & Chainlink Automation.# Lottery-contract
+
+## For development
+> CEI (Checks -> Effects -> Interactions)
+
+First : require(...)...
+Second : Change our own contract state (include events emitting)
+Last : Interact with other contracts
+It's very important for every developer~
 # Lottery-contract
--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.18;
 
 import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 /**
  * @title a contract for raffle
@@ -9,22 +10,33 @@ import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interface
  * @notice for learing solidity
  * @dev implements Chainlink VRF v2
  */
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     error Raffle__NotEnoughFee();
     error Raffle__TimeNotUp();
+    error Raffle__PaymentFailed();
+    error Raffle__NotOpen();
+
+    enum RaffleStatus {
+        OPEN, // 0
+        CALCULATING // 1
+    }
 
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
     uint256 private immutable i_entranceFee;
     uint256 private immutable i_interval;
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
-    uint256 private immutable i_startTime;
+
     bytes32 private immutable i_keyHash;
     uint64 private immutable i_subscriptionId;
     uint32 private immutable i_callbackGasLimit;
+    uint256 private s_startTime;
+    address payable private s_winner;
     address payable[] s_participants;
+    RaffleStatus private s_raffleStatus = RaffleStatus.OPEN;
 
     event EnterRaffle(address indexed participant);
+    event PickWinner(address indexed winner);
 
     constructor(
         uint256 entranceFee_,
@@ -34,10 +46,10 @@ contract Raffle {
         bytes32 keyHash_,
         uint64 subscriptionId_,
         uint32 callbackGasLimit_
-    ) {
+    ) VRFConsumerBaseV2(vrfCoordinator_) {
         i_entranceFee = entranceFee_;
         i_interval = interval_;
-        i_startTime = startTime_;
+        s_startTime = startTime_;
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator_);
         i_keyHash = keyHash_;
         i_subscriptionId = subscriptionId_;
@@ -45,6 +57,9 @@ contract Raffle {
     }
 
     function enterRaffle() external payable {
+        if (s_raffleStatus != RaffleStatus.OPEN) {
+            revert Raffle__NotOpen();
+        }
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughFee();
         }
@@ -53,12 +68,27 @@ contract Raffle {
     }
 
     function pinkWinner() external {
-        if (block.timestamp < i_startTime + i_interval) {
+        if (block.timestamp < s_startTime + i_interval) {
             revert Raffle__TimeNotUp();
         }
-        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+        s_raffleStatus = RaffleStatus.CALCULATING;
+        i_vrfCoordinator.requestRandomWords(
             i_keyHash, i_subscriptionId, REQUEST_CONFIRMATIONS, i_callbackGasLimit, NUM_WORDS
         );
+    }
+
+    function fulfillRandomWords(uint256, uint256[] memory randomWords) internal override {
+        uint256 winnerIndex = randomWords[0] % s_participants.length;
+        address payable winner = s_participants[winnerIndex];
+        s_winner = winner;
+        emit PickWinner(winner);
+        (bool success,) = winner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__PaymentFailed();
+        }
+        s_participants = new address payable[](0); // empty participants array
+        s_startTime = block.timestamp; // reset startTime
+        s_raffleStatus = RaffleStatus.OPEN;
     }
 
     /**
